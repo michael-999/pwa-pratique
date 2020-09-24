0d5eea8b173bc7a1ea73d2c10e06095b5a0dbcc4
michael-999 <michael.benison@hotmail.fr>
2020-09-24 14:21:16 +0200

    3. Service Worker - résultat

--- main.js
+++ main.js
@@ -65,3 +65,14 @@ function loadTechnologies(technos) {
  
 	
 loadTechnologies(technos);
+
+	
+if(navigator.serviceWorker) {
+	
+    navigator.serviceWorker
+	
+        .register('sw.js')
+	
+        .catch(err => console.error('service worker NON enregistré', err));
+	
+}
